c1169b7f2c89fc3ab78856a6572ee0e587a150e8
aeolian <aeolian.eth@protonmail.com>
2022-01-02 08:25:40 +0930

    Add connect wallet button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,12 @@
 export default function Header() {
-  return <header className="h-20 px-8 flex items-center">Pledgr</header>;
+  return (
+    <header className="h-20 px-8 flex items-center justify-between">
+      Pledgr
+      <div>
+        <button className="px-3 py-2 rounded-lg text-white border-2 border-blue-700 text-blue-700">
+          Connect wallet
+        </button>
+      </div>
+    </header>
+  );
 }
